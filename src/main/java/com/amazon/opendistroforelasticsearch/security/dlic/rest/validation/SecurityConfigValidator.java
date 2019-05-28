ec5814b761883308bf7c719666e177138c00c6c5
Shivang Doshi <sdoshi@amazon.com>
2019-05-28 14:35:01 -0700

    Update the license header for newly added files

--- src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/validation/SecurityConfigValidator.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/validation/SecurityConfigValidator.java
@@ -1,15 +1,16 @@
 /*
- * Copyright 2016-2017 by floragunn GmbH - All rights reserved
- * 
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed here is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
- * This software is free of charge for non-commercial and academic use. 
- * For commercial use in a production environment you have to obtain a license 
- * from https://floragunn.com
- * 
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 
 package com.amazon.opendistroforelasticsearch.security.dlic.rest.validation;
