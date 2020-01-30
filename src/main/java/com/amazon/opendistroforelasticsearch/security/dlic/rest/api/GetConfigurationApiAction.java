1411fbe560a1339176a7629bf167a6f58614b451
Sujith Vadakkepat <svadakke@amazon.com>
2020-01-29 18:06:38 -0800

    Version 0.9.0.5

--- src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/api/GetConfigurationApiAction.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/api/GetConfigurationApiAction.java
@@ -54,6 +54,10 @@ public class GetConfigurationApiAction extends AbstractApiAction {
 			final AdminDNs adminDNs, final IndexBaseConfigurationRepository cl, final ClusterService cs,
             final PrincipalExtractor principalExtractor, final PrivilegesEvaluator evaluator, ThreadPool threadPool, AuditLog auditLog) {
 		super(settings, configPath, controller, client, adminDNs, cl, cs, principalExtractor, evaluator, threadPool, auditLog);
+	}
+
+	@Override
+	protected void registerHandlers(RestController controller, Settings settings) {
 		controller.registerHandler(Method.GET, "/_opendistro/_security/api/configuration/{configname}", this);
 		System.out.println("Registering Handler");
 	}
